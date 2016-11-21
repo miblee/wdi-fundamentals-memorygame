fc0e40195053b19429606e05147690ab34ed2ef3
Michelle Lee <michelle@Michelles-Air.home>
2016-11-21 01:03:40 -0800

    wrote game logic to memory game main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,9 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardOne === cardTwo || cardThree === cardFour) {
+	alert("You found a match!");
+}
+else {
+	console.log("Sorry, try again.")
+};
