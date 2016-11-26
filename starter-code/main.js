ef9c0db280034a0359cd3dfebe5a4655dea9f8bd
Michelle Lee <michelle@Michelles-Air.home>
2016-11-25 17:47:59 -0800

    created cards on .js and removed html cards/divs

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,9 +4,23 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo || cardThree === cardFour) {
+/*if (cardOne === cardTwo || cardThree === cardFour) {
 	alert("You found a match!");
 }
 else {
 	console.log("Sorry, try again.")
-};
+};*/
+
+var newCards = document.getElementById('game-board')
+
+
+var createCards = function() {
+	for(var i = 0; i < cards.length; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		board.appendChild(newCard);
+	};
+}
+
+createCards();
+
