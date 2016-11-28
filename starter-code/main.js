01eb686d464d32e81fa033159bb19031edfe137c
Michelle Lee <michelle@Michelles-MacBook-Air.local>
2016-11-28 01:00:42 -0800

    added final game logic to main.js and style.css

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,61 @@
 console.log("JS file is connected to HTML! Woo!");
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king";
+var cardFour = "king";*/
+
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  };
+}
+
+
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161485_960_720.png'>"; // king
+	}
+	else {
+		this.innerHTML = "<img src='https://cdn.pixabay.com/photo/2013/07/13/13/45/playing-card-161484_960_720.png'>"; //queen
+	}
+
+  if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
+}
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  }
+  else {
+    alert("Sorry, try again.");
+  }
+}
+
+createBoard();
+
+/* ---------------------------------------- */
+
+/* var createBoard = function() {
+	for(var i=0; i<cards.length; i++) {
+		cardElement.setAttribute('data-card', cards[i]);
+	}
+}
 
 /*if (cardOne === cardTwo || cardThree === cardFour) {
 	alert("You found a match!");
@@ -10,6 +63,8 @@ var cardFour = "king";
 else {
 	console.log("Sorry, try again.")
 };*/
+
+/*
 
 var newCards = document.getElementById('game-board')
 
@@ -22,5 +77,5 @@ var createCards = function() {
 	};
 }
 
-createCards();
+createCards(); */
 
